741a2568e5c3d443afd9d4fa7abff66f2b0050b2
Kezia Campos <keziacamposcs@gmail.com>
2021-06-17 10:57:39 -0300

    atualização 17.06.21

---
+++ baterponto.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+path = 'ImagensBasicas'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imgread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+def markAttendace(name):
+    whit open('Ponto.cvs', 'r+') as f:
+    myDataList = f.readlines()
+    namelist = []
+    for line in myDataList:
+        entry = line.split(',')
+        namelist.append(entry[0])
+    if name not in namelist:
+        now = datetime.now()
+        dtString = now.strftime('%H:%M:%S')
+        f.writelines(f'\n{name},{dtString}')
+
+    
+
+
+
+encodeListKnow = findEncodings(images)
+print(len(encodeListKnow))
+
+#Webcam
+cap = cv2.VideoCapture(0)
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img,(0,0), None, 0.25,0.25)     
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip (encodeCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnow, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnow, encodeFace)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1,x2,y2,x1 = faceLoc
+            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2), (0,255,0),2)
+            cv2.rectangle(img, (x1,y2-35), (x2,y2),(0,255,0), cv2.FILLED)
+            cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX,1, (255,255,255), 2)
+            markAttendace(name)
+
+    
+    cv2.imgshow('Webcam', img)
+    cv2.waitKey(1)
+
