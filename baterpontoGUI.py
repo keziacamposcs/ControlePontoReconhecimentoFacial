89b3fbaa03faa2ace4e535ea5841a73aa86a30b3
Kezia Campos <32270979+keziacamposcs@users.noreply.github.com>
2021-06-30 14:10:36 -0300

    Add files via upload

---
+++ baterpontoGUI.py
@@ -0,0 +1,99 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+from tkinter import *
+from tkinter import filedialog
+
+def inciar():
+
+    path = 'ImagensBasicas'
+    images = []
+    classNames = []
+    myList = os.listdir(path)
+    print(myList)
+    for cl in myList:
+        curImg = cv2.imread(f'{path}/{cl}')
+        images.append(curImg)
+        classNames.append(os.path.splitext(cl)[0])
+    print(classNames)
+
+    def findEncodings(images):
+        encodeList = []
+        for img in images:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            encode = face_recognition.face_encodings(img)[0]
+            encodeList.append(encode)
+        return encodeList
+
+    def markAttendance(name):
+        with open('Ponto.csv','r+') as f:
+            myDataList = f.readlines()
+            nameList = []
+            for line in myDataList:
+                entry = line.split(',')
+                nameList.append(entry[0])
+            if name not in nameList:
+                now = datetime.now()
+                dtString = now.strftime('%H:%M:%S')
+                f.writelines(f'\n{name},{dtString}')
+        
+
+
+
+    encodeListKnow = findEncodings(images)
+    print(len(encodeListKnow))
+
+    #Webcam
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, img = cap.read()
+        imgS = cv2.resize(img,(0,0), None, 0.25,0.25)     
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodeCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+        for encodeFace, faceLoc in zip (encodeCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnow, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnow, encodeFace)
+            matchIndex = np.argmin(faceDis)
+
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+                print(name)
+                y1,x2,y2,x1 = faceLoc
+                y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+                cv2.rectangle(img,(x1,y1),(x2,y2), (0,255,0),2)
+                cv2.rectangle(img, (x1,y2-35), (x2,y2),(0,255,0), cv2.FILLED)
+                cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX,1, (255,255,255), 2)
+                markAttendance(name)
+
+        
+        cv2.imshow('Webcam',img)
+        cv2.waitKey(1)
+
+def iniciar():
+    global cap
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    visualizar()
+
+def finalizar():
+    global cap
+    cap.release()
+
+
+cap = None
+root = Tk()
+
+btnIniciar  = Button(root, text = "Iniciar", widht=45, command=iniciar)
+btnIniciar.grid(column=0, row=0, padx=5, pady=5)
+
+btnFinalizar = Button(root, text="Finalizar", widht=45)
+btnFinalizar.grid(column=1, row=1, padx=5,pady=5)
+
+lblVideo=Label(root)
+lblVideo.grid(column=0, raw=1, columnspan=2)
+
+root.mainloop()
